aba8a2fee02a99057faa5dc4f1ef3bf4b8d06035
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-06 19:41:23 +0000

    Add files via upload

---
+++ exercice2/index.js
@@ -0,0 +1,16 @@
+let elementDiv = document.querySelector("div");
+elementDiv.style.background = "cyan";
+elementDiv.style.padding= "5px";
+
+let elementli = document.querySelectorAll('li')[1];
+elementli.style.border = "2px solid green";
+
+ document.body.style.fontSize = "2em";
+
+ const divbackground = getComputedStyle(elementDiv).backgroundColor;
+ if (divbackground === "rgb(173,216, 230") {
+    console.log("good mornig x and y")
+ }
+
+
+
